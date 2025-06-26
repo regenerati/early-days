e6134e9348773cae4600a14b642fd00d16b185f3
Chelsea Matthews <cmatt@CM-Laptop.local>
2025-06-26 14:59:20 -0700

    Remove index

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Hello from Next.js!</h1>
-      <p>This is your first page 🎉</p>
-    </div>
-  );
-}
-<img src="/sunrise image.png" alt="sunrise" width="400" />
